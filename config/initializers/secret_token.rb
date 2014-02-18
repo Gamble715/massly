3722a3cb79accc95f34e2aa58fcf1222b6990e6b
MakerSquare Student <students@themakersquare.com>
2014-02-18 09:12:26 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Massly::Application.config.secret_key_base = 'a76cd11a6aaeb1eac1723ae72057e5e9fc51d4667e88a422eeac184f5d2b5fd73558e9642760ac555491e32dd5b111a123e09bb3ba27f2a581f02c58bfa86494'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Massly::Application.config.session_store :cookie_store, key: '_massly_session'
